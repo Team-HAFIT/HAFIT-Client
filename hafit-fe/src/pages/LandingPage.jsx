1210b01681873f7d508af8d15b61acb05227688f
BaegJeonghoon <bb_jhoon@naver.com>
2023-05-03 16:14:45 +0900

    HF-90 [Feat] 로그인 페이지 레이아웃 구성 2023/05/03 - 16:12
    로그인 페이지 레이아웃 구성
    * 로그인 폼 정렬 문제는 추후 해결 할 것 *

--- hafit-fe/src/pages/LandingPage.jsx
+++ hafit-fe/src/pages/LandingPage.jsx
@@ -8,68 +8,68 @@ import Feature1 from "../assets/img/poseEstimation.png";
 
 function LandingPage() {
   return (
-    <div>
-      {/* Header */}
-      <div className="header">
-        {" "}
-        {/* 
-          - # 2023. 5. 2. 화요일
-          - 임시 문구이며 추후 수정 필요
-          - 폰트, 색상, 사이즈 수정 필요
-          - 버튼은 추후 디자인 수정 예정, component로 분리하여 사용할 예정
-          - 정렬, 마진 등은 추후 수정 예정
-        */}
-        <h3>AI 헬스 메이트, 해핏 ˙ᵕ˙</h3>
-        <div>
-          <h1>HAFIT</h1>
+    <div className="top-container">
+      <div className="body-wrapper">
+        {/* Header */}
+        <div className="header">
+          {" "}
+          {/* 
+            - # 2023. 5. 2. 화요일
+            - 임시 문구이며 추후 수정 필요
+            - 폰트, 색상, 사이즈 수정 필요
+            - 버튼은 추후 디자인 수정 예정, component로 분리하여 사용할 예정
+            - 정렬, 마진 등은 추후 수정 예정
+          */}
+          <h3>AI 헬스 메이트, 해핏 ˙ᵕ˙</h3>
+          <div>
+            <h1>HAFIT</h1>
+          </div>
+          <p>AI 분석을 통해 운동 자세 교정을 돕고, 개수와 세트수를 대신 세어드릴게요!</p>
+          <Button className="btn-start" style={{color: "white"}}>
+            해핏 시작하기
+          </Button>
         </div>
-        <p>AI 분석을 통해 자세를 교정해주고, 개수와 세트수를 대신 세어드릴게요!</p>
-        <Button type="primary" size="large" style={{marginTop: "156px", marginBottom:"16px", fontSize: "16px", fontWeight: "500", padding: "24px", display:"flex", alignItems: "center", backgroundColor: "rgb(108, 0, 240)"}}>
-          해핏 시작하기
-        </Button>
-      </div>
 
-      {/* Feature */}
-      <div className="container">
-        {" "}
-        <h2>Feature</h2>
-        <Row gutter={32}>
-          <Col span={8} className="feature">
-            {" "}
-            <img src={Feature1} alt="기능 1" />
-            <h3>Feature 1</h3>
-            <p>
-              올바른 자세를 
-            </p>
-          </Col>
-          <Col span={8} className="feature">
-            {" "}
-            {/* 클래스 이름으로 스타일 적용 */}
-            <img src="feature-icon-2.svg" alt="기능 2" />
-            <h3>Feature 2</h3>
-            <p>
-              Ut ac facilisis ante, in pellentesque felis. Integer arcu nisl,
-              eleifend at pretium id, pulvinar nec sapien.
-            </p>
-          </Col>
-          <Col span={8} className="feature">
-            {" "}
-            {/* 클래스 이름으로 스타일 적용 */}
-            <img src="feature-icon-3.svg" alt="기능e 3" />
-            <h3>Feature 3</h3>
-            <p>
-              Aliquam erat volutpat. Mauris fermentum tincidunt lacus, sed
-              fermentum enim vehicula ac.
-            </p>
-          </Col>
-        </Row>
+        {/* Feature */}
+        <div className="container">
+          {" "}
+          <h2>Feature</h2>
+          <Row gutter={32}>
+            <Col span={8} className="feature">
+              {" "}
+              <img src={Feature1} alt="기능 1" />
+              <h3>Feature 1</h3>
+              <p>
+                올바른 자세를 
+              </p>
+            </Col>
+            <Col span={8} className="feature">
+              {" "}
+              <img src="feature-icon-2.svg" alt="기능 2" />
+              <h3>Feature 2</h3>
+              <p>
+                Ut ac facilisis ante, in pellentesque felis. Integer arcu nisl,
+                eleifend at pretium id, pulvinar nec sapien.
+              </p>
+            </Col>
+            <Col span={8} className="feature">
+              {" "}
+              <img src="feature-icon-3.svg" alt="기능 3" />
+              <h3>Feature 3</h3>
+              <p>
+                Aliquam erat volutpat. Mauris fermentum tincidunt lacus, sed
+                fermentum enim vehicula ac.
+              </p>
+            </Col>
+          </Row>
+        </div>
       </div>
 
       {/* Footer */}
       <div className="footer">
         {" "}
-        {/* 클래스 이름으로 스타일 적용 */}
-        <p>© 2022 Your Awesome App Name. All rights reserved.</p>
+        <hr style={{background: "#dbdbdb", height: "1px", border: "0"}} />
+        <p style={{color:"#999999"}}>© 2023 HAFIT. All rights reserved.</p>
       </div>
     </div>
   );
--- hafit-fe/src/pages/LoginPage.jsx
+++ hafit-fe/src/pages/LoginPage.jsx
@@ -1,12 +1,127 @@
 import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+
+import "../styles/loginPage.css";
+
+import kakao from "../assets/img/sns-icons/kakao-icon.png";
+import google from "../assets/img/sns-icons/google-icon.png";
+import naver from "../assets/img/sns-icons/naver-icon.png";
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
 
 function LoginPage() {
-
   return (
-    <div>
-        로그인
+    <div className="top-container">
+      <div className="body-wrapper">
+        <div className="header-login">
+          <h1>로그인</h1>
+        </div>
+        <div className="login-container">
+          <Form
+            name="basic"
+            labelCol={{
+              span: 0,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+              width: '100%',
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="on"
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "이메일을 입력해주세요!",
+                },
+              ]}
+            >
+              <Input
+                placeholder="이메일 입력"
+                style={{ height: "4rem", fontSize: "1rem" }}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "비밀번호를 입력해주세요!",
+                },
+              ]}
+            >
+              <Input.Password
+                placeholder="비밀번호 입력"
+                style={{ height: "4rem", fontSize: "1rem" }}
+              />
+            </Form.Item>
+            <Form.Item
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100px"
+              }}
+            >
+              <Form.Item name="remember" valuePropName="checked">
+                <Checkbox>자동 로그인</Checkbox>
+              </Form.Item>
+                <a href="/forgot-password" style={{ marginLeft: "auto" }}>비밀번호 찾기</a>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 6,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                로그인
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+        <div className="sns-login">
+          {/* 카카오톡, 구글, 네이버 아이콘이 나란히 있는 SNS 간편 로그인 구현 */}
+          <Link to="/" className="kakao-login">
+            <img src={kakao} alt="카카오 로그인" style={{width: '90%'}} />
+          </Link>
+          <Link to="/" className="google-login">
+            <img src={google} alt="구글 로그인" style={{width: '90%'}} />
+          </Link>
+          <Link to="/" className="naver-login">
+            <img src={naver} alt="네이버 로그인" style={{width: '90%'}} />
+          </Link>
+        </div>
+        <div className="sign-up" style={{textAlign: "center"}}>
+          <span>아직 회원이 아니신가요?</span> <a href="/join">회원가입</a>
+        </div>
+
+        {/* Footer */}
+        <div className="footer">
+          {" "}
+          <hr style={{background: "#dbdbdb", height: "1px", border: "0"}} />
+          <p style={{color:"#999999"}}>© 2023 HAFIT. All rights reserved.</p>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default LoginPage;
