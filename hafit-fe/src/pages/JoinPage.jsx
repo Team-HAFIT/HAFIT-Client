c14deb17e93ef7f726f1826fca56382bd82650f8
BaegJeonghoon <bb_jhoon@naver.com>
2023-05-05 20:43:45 +0900

    HF-109 [Feat] 회원가입 axios 통신 구현 2023/05/05 - 18:43

--- hafit-fe/src/pages/JoinPage.jsx
+++ hafit-fe/src/pages/JoinPage.jsx
@@ -9,6 +9,8 @@ import {
   Typography,
 } from "antd";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 import PhoneNumberInput from "../components/inputs/phoneNumberInput";
 
@@ -18,12 +20,37 @@ const { Option } = Select;
 const { Title } = Typography;
 
 const JoinPage = () => {
+  const [loading, setLoading] = useState(false); // 요청 중 여부 상태 저장용 state
+  const navigate = useNavigate(); // 페이지 이동을 위해 useNavigate hook 사용
+
+  const onFinish = (values) => {
+    setLoading(true); // 요청 시작 시 로딩 중 상태로 설정
+
+    axios
+      .post("http://172.26.8.73:8080/user/join", JSON.stringify(values), {
+        headers: {
+          "Content-Type": "application/json", // 요청 헤더에 Content-Type 설정
+        },
+      })
+      .then((response) => {
+        console.log(response.data); // 응답 결과 출력
+
+        navigate("/intro"); // 로그인 성공 시 메인 페이지로 이동
+      })
+      .catch((error) => {
+        console.error(error.response.data);
+      })
+      .finally(() => {
+        setLoading(false); // 요청 종료 시 로딩 중 상태 해제
+      });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   const [form] = Form.useForm();
   const [agreed, setAgreed] = useState(false);
-
-  const handleSubmit = (values) => {
-    console.log("수신된 양식 값: ", values);
-  };
 
   const handleAgreeChange = (e) => {
     setAgreed(e.target.checked);
@@ -34,7 +61,10 @@ const JoinPage = () => {
       <Row justify="center" align="middle" style={{ height: "100vh" }} >
         <Col span={12} align="middle">
           <Title level={3}>회원가입</Title>
-          <Form form={form} onFinish={handleSubmit} layout="vertical">
+          <Form form={form}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          layout="vertical">
             <Form.Item
               label="이메일"
               name="email"
@@ -72,12 +102,12 @@ const JoinPage = () => {
                   message: "비밀번호를 입력해주세요",
                 },
                 {
-                  min: 6,
-                  message: "비밀번호는 최소 6자리 이상이어야 합니다",
+                  min: 8,
+                  message: "비밀번호는 최소 8자리 이상이어야 합니다",
                 },
               ]}
             >
-              <Input.Password />
+              <Input.Password placeholder="영문, 숫자, 특수문자 포함 8자 이상" />
             </Form.Item>
             <Form.Item
               label="비밀번호 확인"
@@ -136,7 +166,13 @@ const JoinPage = () => {
               </Checkbox>
             </Form.Item>
             <Form.Item>
-              <Button type="primary" htmlType="submit" disabled={!agreed}>
+              <Button 
+              type="primary" 
+              htmlType="submit" 
+              disabled={!agreed}
+              loading={loading}
+              block
+            >
                 가입하기
               </Button>
             </Form.Item>
