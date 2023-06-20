4e8944c0ae54132fc571f1e489b412c2ad465cf1
BaegJeonghoon <bb_jhoon@naver.com>
2023-06-21 05:22:34 +0900

    design: 스쿼트 세팅 페이지 css 수정(임시)

--- hafit-fe/src/pages/exercises/squat/SquatSetting.jsx
+++ hafit-fe/src/pages/exercises/squat/SquatSetting.jsx
@@ -1,62 +1,66 @@
 import React, { useState } from "react";
 import { Form, InputNumber, Button, Card, Row, Col, Typography } from "antd";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import { useSelector } from "react-redux";
+
+import "../../../styles/pages/exercise/execSetting.css";
 
 // import Header from "../../../components/MainNavbar";
 
 const { Title } = Typography;
 
 const SqautSetting = () => {
-    const [loading, setLoading] = useState(false); // 요청 중 여부 상태 저장용 state
-    const navigate = useNavigate(); // 페이지 이동을 위해 useNavigate hook 사용
-    
-    const accessToken = useSelector((state) => state.authToken.accessToken);
+  const [loading, setLoading] = useState(false); // 요청 중 여부 상태 저장용 state
+  const navigate = useNavigate(); // 페이지 이동을 위해 useNavigate hook 사용
 
-    const onFinish = (values) => {
-        setLoading(true); // 요청 시작 시 로딩 중 상태로 설정
-      
-        const updatedValues = {...values, exerciseId: 1};
-        axios
-          .post("/api/plans", updatedValues, {
-            headers: {
-              "Content-Type": "application/json", // 요청 헤더에 Content-Type 설정
-              "Authorization": `Bearer ${accessToken}`,
-            },
-            timeout: 5000, // 요청 제한 시간 설정
-          })
-          .then((response) => {
-            console.log(response.data); // 응답 결과 출력
-            
-            const planId = response.data.planId; // 응답 데이터에서 planId 추출
-            navigate("/exec/squat", { state: { planId } }); // 이동 시, planId 값을 함께 전달
-          })
-          .catch((error) => {
-            console.error(error);
-            console.log(error.response);
-            // if (error.response && error.response.status === 400) {
-            //   alert("값을 확인해주세요.");
-            // } else {
-            //   alert("에러 발생"); // 기타 에러 발생 시 에러 메시지 띄우기
-            // }
-          })
-          .finally(() => {
-            setLoading(false); // 요청 종료 시 로딩 중 상태 해제
-          });
-      };
+  const accessToken = useSelector((state) => state.authToken.accessToken);
 
-      const onFinishFailed = (errorInfo) => {
-        console.log("Failed:", errorInfo);
-      };
+  const onFinish = (values) => {
+    setLoading(true); // 요청 시작 시 로딩 중 상태로 설정
+
+    const updatedValues = { ...values, exerciseId: 1 };
+    axios
+      .post("/api/plans", updatedValues, {
+        headers: {
+          "Content-Type": "application/json", // 요청 헤더에 Content-Type 설정
+          Authorization: `Bearer ${accessToken}`,
+        },
+        timeout: 5000, // 요청 제한 시간 설정
+      })
+      .then((response) => {
+        console.log(response.data); // 응답 결과 출력
+
+        const planId = response.data.planId; // 응답 데이터에서 planId 추출
+        navigate("/exec/squat", { state: { planId } }); // 이동 시, planId 값을 함께 전달
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log(error.response);
+        // if (error.response && error.response.status === 400) {
+        //   alert("값을 확인해주세요.");
+        // } else {
+        //   alert("에러 발생"); // 기타 에러 발생 시 에러 메시지 띄우기
+        // }
+      })
+      .finally(() => {
+        setLoading(false); // 요청 종료 시 로딩 중 상태 해제
+      });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
   return (
     <div>
       {/* <Header /> */}
       <Row justify="center" style={{ height: "100vh", marginTop: "4rem" }}>
-        <Col span={12} align="middle">
-          <Title level={2} align="middle">스쿼트</Title>
-
+        <Col span={16} align="middle">
+          <Title level={2} align="left">
+            <span style={{ fontWeight: "bold" }}>스쿼트</span>
+            <span style={{ marginLeft: "8px", fontSize: "21px" }}>| squat</span>
+          </Title>
           <Card>
             <Form
               name="exercise"
@@ -76,7 +80,13 @@ const SqautSetting = () => {
                   },
                 ]}
               >
-                <InputNumber min={1} max={100} step={5} />
+                <InputNumber
+                  className="exec-input"
+                  min={1}
+                  max={100}
+                  step={5}
+                  addonAfter="개"
+                />
               </Form.Item>
 
               <Form.Item
@@ -89,7 +99,12 @@ const SqautSetting = () => {
                   },
                 ]}
               >
-                <InputNumber min={1} max={20} />
+                <InputNumber
+                  className="exec-input"
+                  min={1}
+                  max={20}
+                  addonAfter="세트"
+                />
               </Form.Item>
 
               <Form.Item
@@ -103,7 +118,13 @@ const SqautSetting = () => {
                 ]}
               >
                 {/* step은 숫자의 증감 단위를 설정하는데, 이 경우 5씩 증감합니다.*/}
-                <InputNumber min={1} max={100} step={5} />
+                <InputNumber
+                  className="exec-input"
+                  min={1}
+                  max={100}
+                  step={5}
+                  addonAfter="kg"
+                />
               </Form.Item>
 
               <Form.Item
@@ -116,13 +137,24 @@ const SqautSetting = () => {
                   },
                 ]}
               >
-                {/* suffix는 숫자 뒤에 추가적인 문자나 기호를 넣을 때 사용합니다. 이 경우에는 초(s)를 붙입니다. */}
-                <InputNumber min={0} max={300} step={10} />
+                <InputNumber
+                  className="exec-input"
+                  min={0}
+                  max={1000}
+                  step={10}
+                  // suffix="초"
+                  addonAfter="초"
+                />
               </Form.Item>
 
               <Form.Item>
-                <Button type="primary" htmlType="submit" loading={loading}>
-                  시작하기
+                <Button
+                  className="btn-submit"
+                  type="primary"
+                  htmlType="submit"
+                  loading={loading}
+                >
+                  운동 시작하기
                 </Button>
               </Form.Item>
             </Form>
