063cb5a6e9214321e12c4fc88983de632e873349
ooyniz <ooyniz@gmail.com>
2023-06-26 00:15:43 +0900

    feat: 루틴 생성

--- hafit-fe/src/pages/exercises/calendar/CalendarPage.jsx
+++ hafit-fe/src/pages/exercises/calendar/CalendarPage.jsx
@@ -13,7 +13,6 @@ const CalendarPage = () => {
   const [targetReps, setTargetReps] = useState('');
   const [targetSets, setTargetSets] = useState('');
   const [targetWeight, setTargetWeight] = useState('');
-  const [repeatDays, setRepeatDays] = useState([]);
   const [exerciseSchedules, setExerciseSchedules] = useState([]);
   const [data, setData] = useState([]);
   const [showModal, setShowModal] = useState(false);
@@ -22,6 +21,13 @@ const CalendarPage = () => {
   const [keywords, setKeywords] = useState([]);
   const [exerciseList, setExerciseList] = useState([]);
   const [selectedExerciseId, setSelectedExerciseId] = useState(null);
+  const [routineCount, setRoutineCount] = useState('');
+  const [routineSet, setRoutineSet] = useState('');
+  const [routineWeight, setRoutineWeight] = useState('');
+  const [exerciseId, setExerciseId] = useState('');
+  const [goalId, setGoalId] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [repeatDays, setRepeatDays] = useState([]);
 
   const fetchGoals = useCallback(async () => {
 
@@ -128,13 +134,31 @@ const CalendarPage = () => {
     try {
       values.exerciseId = selectedExerciseId;
       values.goal_target_date = values.goal_target_date.format('YYYY-MM-DD');
-      const response = await axios.post('/api/goals', values, {
+      const goalResponse = await axios.post('/api/goals', values, {
         headers: {
           'Content-Type': 'application/json',
           authorization: `Bearer ${accessToken}`,
         },
         timeout: 10000,
       });
+
+      console.log(goalResponse.data);
+      const routineData = {
+        ...values,
+        goalId: goalResponse.data,
+      };
+      console.log(routineData);
+      console.log(routineData);
+      console.log(routineData);
+      console.log(routineData);
+      await axios.post('/api/routines', routineData, {
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${accessToken}`,
+        },
+        timeout: 10000,
+      });
+
       window.location.reload();
     } catch (error) {
       console.log(error);
@@ -154,6 +178,34 @@ const CalendarPage = () => {
     };
     setExerciseSchedules([...exerciseSchedules, newSchedule]);
     setModalVisible(false);
+  };
+
+  const handleRoutineCountChange = (e) => {
+    setRoutineCount(e.target.value);
+  };
+
+  const handleRoutineSetChange = (e) => {
+    setRoutineSet(e.target.value);
+  };
+
+  const handleRoutineWeightChange = (e) => {
+    setRoutineWeight(e.target.value);
+  };
+
+  const handleExerciseIdChange = (e) => {
+    setExerciseId(e.target.value);
+  };
+
+  const handleGoalIdChange = (e) => {
+    setGoalId(e.target.value);
+  };
+
+  const handleStartDateChange = (e) => {
+    setStartDate(e.target.value);
+  };
+
+  const handleRepeatDaysChange = (checkedValues) => {
+    setRepeatDays(checkedValues);
   };
 
   const exerciseOptions = {
@@ -178,10 +230,6 @@ const CalendarPage = () => {
 
   const handleTargetWeightChange = (e) => {
     setTargetWeight(e.target.value);
-  };
-
-  const handleRepeatDaysChange = (checkedValues) => {
-    setRepeatDays(checkedValues);
   };
 
   const disabledDate = (current) => {
@@ -282,7 +330,7 @@ const CalendarPage = () => {
         >
           <Form onFinish={handleGoalSubmit}>
             <Form.Item name="goal_target_date" label="목표 날짜" rules={[{ required: true, message: 'D-Day를 선택해 주세요.' }]}>
-              <DatePicker />
+              <DatePicker disabledDate={disabledDate}/>
             </Form.Item>
             <Form.Item name="goal_content" label="운동 목표" rules={[{ required: true, message: '운동 목표를 입력해 주세요.' }]}>
               <Input />
@@ -310,6 +358,55 @@ const CalendarPage = () => {
                   {exercise.exerciseName}
                 </Button>
               ))}
+            </Form.Item>
+            <Form.Item label="목표 개수">
+            <Input.Group compact>
+              <Form.Item
+                name="routineCount"
+                noStyle
+                rules={[{ required: true, message: '목표 개수를 입력해주세요' }]}
+              >
+                <Input
+                  style={{ width: '50%' }}
+                  placeholder="개"
+                  type="number"
+                  min={1}
+                  onChange={handleRoutineCountChange}
+                />
+              </Form.Item>
+              <Form.Item
+                name="routineSet"
+                noStyle
+                rules={[{ required: true, message: '목표 세트 수를 입력해주세요' }]}
+              >
+                <Input
+                  style={{ width: '50%' }}
+                  placeholder="세트"
+                  type="number"
+                  min={1}
+                  onChange={handleRoutineSetChange}
+                />
+                </Form.Item>
+              </Input.Group>
+            </Form.Item>
+            <Form.Item label="목표 중량" name="routineWeight"  required={false}>
+              <Input
+                placeholder="kg"
+                type="number"
+                min={1}
+                onChange={handleRoutineWeightChange}
+              />
+            </Form.Item>
+            <Form.Item label="반복 요일" name="repeatDays">
+              <Checkbox.Group onChange={handleRepeatDaysChange}>
+                <Checkbox value="MONDAY">월</Checkbox>
+                <Checkbox value="TUESDAY">화</Checkbox>
+                <Checkbox value="WEDNESDAY">수</Checkbox>
+                <Checkbox value="THURSDAY">목</Checkbox>
+                <Checkbox value="FRIDAY">금</Checkbox>
+                <Checkbox value="SATURDAY">토</Checkbox>
+                <Checkbox value="SUNDAY">일</Checkbox>
+              </Checkbox.Group>
             </Form.Item>
             <Form.Item>
               <Button type="primary" htmlType="submit" style={{ backgroundColor: '#8A2BE2' }}>확인</Button>
